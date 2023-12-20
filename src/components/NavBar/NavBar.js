a501e642752984fcfe34d0401c4cb189915ae7b3
ShannyAssa <shannyas@mta.ac.il>
2023-12-20 22:01:12 +0200

    navigation bar CSS

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,11 +5,13 @@ import './NavBar.css';
 const NavBar = () => {
   return (
     <nav className="nav-class">
-      <Link className="nav-link" to="/">Home</Link>
-      <Link className="nav-link" to="/about">About</Link>
-      <Link className="nav-link" to="/signup">Signup</Link>
-      <Link className="nav-link" to="/login">Login</Link>
-
+      <h2 className='nav-title'>Heavenly Horizon Beauty Salon & Spa</h2>
+      <div className="nav-links">
+        <Link className="nav-link" to="/">Home</Link>
+        <Link className="nav-link" to="/about">About</Link>
+        <Link className="nav-link" to="/signup">Signup</Link>
+        <Link className="nav-link" to="/login">Login</Link>
+      </div>
     </nav>
     );
 }
