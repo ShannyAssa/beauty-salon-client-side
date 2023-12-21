b9a2cafa47a5940a55d5ef7c661ebb45c1c5d734
ShannyAssa <shannyas@mta.ac.il>
2023-12-21 19:53:41 +0200

    changes some CSS , icons, etc.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
+import logoDefault from '../../resources/logo-64px.png';
+import logoHover from '../../resources/logo2-64px.png';
 
 const NavBar = () => {
   return (
     <nav className="nav-class">
+      <img className="nav-icon" 
+        src={logoDefault}
+        onMouseOver={(e) => (e.currentTarget.src = logoHover)}
+        onMouseOut={(e) => (e.currentTarget.src = logoDefault)}
+      ></img>
       <h2 className='nav-title'>Heavenly Horizon Beauty Salon & Spa</h2>
       <div className="nav-links">
         <Link className="nav-link" to="/">Home</Link>
